1783b4418078c7bd9b5d509e803902bd82fbcfaa
Flonicah <bsc-com-28-19@unima.ac.mw>
2023-07-13 05:09:27 -0700

    added operational code

--- Program.cs
+++ Program.cs
@@ -13,3 +13,22 @@ Console.WriteLine( firstname + "  sold" +  widgetsol + "  widgets");
 // calculating the modulus forms here of the division operation
 Console.WriteLine($"modulus of  200/5 :  {200%5}");
 Console.WriteLine($"the modulus of 7/5 : {7%5}");
+// more to go with coding be happy
+// the increment and decrement operations code
+int value =0;
+value = value + 5;
+value += 5;
+Console.WriteLine("The first value is " + value);
+value++;
+Console.WriteLine("The second value " + value);
+value-=1;
+Console.WriteLine("The third value is " + value);
+value--;
+Console.WriteLine("The fourth value is " + value);
+
+// going into another step
+
+int fareinheit = 94;
+decimal celsius =(fareinheit - 32m) * (5m/9m);
+Console.WriteLine("The temperature is " + celsius + "celsius");
+
