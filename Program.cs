93a8643c45dc72eca79c63fac736f501be8ac325
Flonicah <bsc-com-28-19@unima.ac.mw>
2023-07-13 03:33:41 -0700

    tough to push though

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿// More practise on the Csharp programming 
+int x = 11;
+string updateText = "Update to Windows";
+string information =$"{updateText} {x}";
+Console.WriteLine(information);
+// performing the operations of values and more calculations
+int firstnumber =17;
+int secondnumber  =3;
+Console.WriteLine(firstnumber + secondnumber);
+int widgetsol = 7;
+string firstname ="Chris";
+Console.WriteLine( firstname + "  sold" +  widgetsol + "  widgets");
+// calculating the modulus forms here of the division operation
+Console.WriteLine($"modulus of  200/5 :  {200%5}");
+Console.WriteLine($"the modulus of 7/5 : {7%5}");
